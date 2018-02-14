22556e1a983b3822eec44d6dfe66c9dbdc1618d4
Adam Ryan <C16302271@mydit.ie>
2018-02-14 16:43:57 +0000

    Created Tune class with private fields, public accessors and public toString() method. Created TuneBook.java .

---
+++ LabTest2/ie/dit/Tune.java
@@ -0,0 +1,44 @@
+package ie.dit;
+
+public class Tune{
+
+	//Private fields
+	private int x;
+	private String title;
+	private String altTitle;
+	private String notation;
+
+	//Public accessor methods
+	public int getX()
+	{
+		return x;
+	}
+
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public String getAltTitle()
+	{
+		return altTitle;
+	}
+
+	public String getNotation()
+	{
+		return notation;
+	}
+
+	//toString() method
+	public String toString()
+	{
+		if(altTitle == null)
+		{
+			return x + ", " + title;
+		}
+		else
+		{
+			return x + ", " + title + ", " + altTitle;
+		}
+	}
+}
